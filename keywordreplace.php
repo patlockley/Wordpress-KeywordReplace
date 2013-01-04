722671da9df690144f984d63c7e875393a5b0580
patlockley <patrick.lockley@googlemail.com>
2013-01-04 22:19:25 +0000

    First Commit
    First commit

---
+++ keywordreplace.php
@@ -0,0 +1,162 @@
+<?PHP
+
+/*
+
+Plugin Name: Keyword replace
+description: add licensing info to a Wordpress blog or post
+Version: 0.1
+Author: pgogy
+Author URI: http://www.pgogy.com
+
+*/
+
+class keyword_replace{
+
+	function keywordreplace_options_page() {
+	  ?>
+		<div class="wrap">
+		<h2>Keyword Replace</h2>
+		<form method="post" action="">
+		<?php 
+			
+				wp_nonce_field('keywordreplace','keywordreplace');
+				settings_fields( 'keywordreplace' );
+		
+		?><p>Which categories don't you want to replace (a checked box means the category won't be replaced)</p>
+		<?PHP
+		
+			$args=array(
+				'orderby' => 'name',
+				'order' => 'ASC'
+			);
+			
+			$categories=get_categories($args);
+			
+			$list = explode(",", get_option('categories_exclude'));
+			
+			foreach($categories as $category) { 
+			
+				echo "<p><label>" . $category->name . "</label> - <input name='category_" . $category->term_id . "' type='checkbox' ";
+				
+				if(in_array($category->term_id, $list)){
+				
+					echo " checked ";
+				
+				}
+				
+				echo "/></p>";
+			
+			}
+		
+		?>
+		<p>Enter a word, then a comma, then a link to replace it with - say Wordpress,http://wordpress.org</p>
+		<p>Separate words with a carriage return (press enter or return)</p>
+		<textarea rows="15" cols="100" name="urlpairs" ><?php 
+		
+																$string = get_option('urlpairs');
+																
+																if($string==""){
+																
+																	echo "Wordpress,http://wordpress.org\n";
+																
+																}else{
+																
+																	echo $string;
+																
+																}
+																 
+																
+														?></textarea>    
+		<p class="submit">
+		<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+		</p>
+		</form>
+	</div>
+	  
+	  <?php
+	}
+
+	function keywordreplace_postform(){
+	
+		if (!empty($_POST['keywordreplace'])){
+
+			if(!wp_verify_nonce($_POST['keywordreplace'],'keywordreplace') ){
+			
+				print 'Sorry, your nonce did not verify.';
+				exit;
+				
+			}else{	
+
+				update_option("urlpairs",$_POST['urlpairs']);
+				
+				$categories = array();
+			
+				foreach($_POST as $key => $value){
+				
+					if($value=="on"){
+					
+						array_push($categories, substr($key,strlen($key)-1,1));
+					
+					}
+					
+				}
+				
+				update_option("categories_exclude",implode(",", $categories));
+								
+			}
+			
+		}
+		
+	}
+
+	function register_textswap() {
+		register_setting( 'keywordreplace', 'urlpairs' );
+		register_setting( 'keywordreplace', 'categories_exclude' );
+	}
+
+	function swap_text($output){
+	
+		$replace = explode("\n",get_option('urlpairs')); 
+		
+		$text = $output;
+		
+		while($pair = array_shift($replace)){
+		
+			$data = explode(",",$pair);
+			
+			$text = preg_replace("/\b(" . $data[0] . ")\b/","<a href=\"" . $data[1] . "\">" . $data[0] . "</a>", $text);
+		
+		}
+		
+		$args=array(
+			'orderby' => 'name',
+			'order' => 'ASC',
+			'exclude' => get_option('categories_exclude')
+		);
+		
+		$categories=get_categories($args);
+		
+		foreach($categories as $category) { 
+		
+			$text = preg_replace("/\b(" . $category->name . ")\b/"," <a href=\"" . get_category_link( $category->term_id ) . "\">" . $category->name . "</a>", $text);
+		
+		}
+	
+		return $text;
+	
+	}
+
+	function swap_menu_option() {
+	  add_options_page('Keyword Replace Options', 'Keyword Options', 'manage_options', 'keywordreplace', array($this,'keywordreplace_options_page'));
+	}
+
+}
+
+$keyword_replace = new keyword_replace;
+
+add_action('admin_init', array($keyword_replace,'register_textswap') );
+add_action('admin_menu', array($keyword_replace,'swap_menu_option'));
+add_filter( "the_content", array($keyword_replace,"swap_text") );
+add_action('admin_head', array($keyword_replace,'keywordreplace_postform'));
+
+?>
---
+++ readme.txt
@@ -0,0 +1,30 @@
+=== Plugin Name ===
+Contributors: pgogy
+Tags: keyword replace, category replace
+Requires at least: 3.0.0
+Tested up to: 3.5
+Stable tag: 0.1
+
+Allows a user to automatically replace category mentions in blog content with a link to that category page on the blog. Also allows any word to be turned into a link in the blog's content.
+
+== Description ==
+
+Allows a user to automatically replace category mentions in blog content with a link to that category page on the blog. Also allows any word to be turned into a link in the blog's content.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload the contents of the zip (including the folder) to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Visit the settings page for the plugin - No other configuration is required.
+
+== Changelog ==
+
+== Screenshots ==
+
+== Frequently Asked Questions ==
+
+We are a new plugin - so no questions so far.
